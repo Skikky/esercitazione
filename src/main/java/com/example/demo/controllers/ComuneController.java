03c890689fa5c9a3324715c55de79be0bd9eb4ac
Skikky <skikky00@gmail.com>
2024-05-31 18:37:36 +0200

    implementata sicurezza

--- src/main/java/com/example/demo/controllers/ComuneController.java
+++ src/main/java/com/example/demo/controllers/ComuneController.java
@@ -31,18 +31,21 @@ public class ComuneController {
         return ResponseEntity.ok(comuni);
     }
 
+    @Secured({"ADMIN"})
     @PostMapping("/create")
     public ResponseEntity<Comune> createComune(@RequestBody Comune comune) {
         Comune newComune = comuneService.create(comune);
         return ResponseEntity.ok(newComune);
     }
 
+    @Secured({"ADMIN"})
     @PutMapping("/update/{id}")
     public ResponseEntity<Comune> updateComune(@PathVariable Long id, @RequestBody Comune comuneDetails) {
         Comune updatedComune = comuneService.update(id, comuneDetails);
         return ResponseEntity.ok(updatedComune);
     }
 
+    @Secured({"ADMIN"})
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteComune(@PathVariable Long id) {
         comuneService.deleteComune(id);
